e49d8ce1395698a76adac9c0fb101bb684d90a66
Kevin Streiter <kevin.streiter@gmail.com>
2020-04-12 10:45:47 +0200

    webpack css bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,47 @@
 const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './public/static/scripts/script.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './public/static/scripts/build')
+        path: path.resolve(__dirname, 'public/static/build')
     },
     module: {
         rules: [
             {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                },
             },
         ],
     },
+    plugins: [
+
+        new MiniCssExtractPlugin({
+            filename: "bundle.css"
+        })
+    ]
 }
